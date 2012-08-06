d6fe58c039f2241930394d341c13b6776b236894
Eyrie <eyriechen@gmail.com>
2012-08-06 21:09:13 +0800

    big bang

---
+++ vendors/vendors.js
@@ -0,0 +1,52 @@
+jQuery(document).ready(function() {
+    jQuery('#datepicker').datepicker();
+    refresh_table();
+    Drupal.behaviors.VendorsBehaviour = {
+        attach: function(context, settings) {  
+            jQuery('#-add-order-form').ajaxComplete(function(event, xhr, settings) {
+                refresh_table();
+            });
+        }
+    }
+
+
+});
+    
+function refresh_table(){
+    jQuery(".form_amount input").unbind("keyup");
+    jQuery(".form_amount input").unbind("mouseleave");
+    jQuery(".form_price input").unbind("keyup");
+    jQuery(".form_price input").unbind("mouseleave");
+    jQuery('.form_amount input').keyup(function() {
+        change_sum(this);
+    });
+    jQuery('.form_amount input').mouseleave(function() {
+        change_sum(this);
+    });
+    jQuery('.form_price input').keyup(function() {
+        change_sum(this);
+    });
+    jQuery('.form_price input').mouseleave(function() {
+        change_sum(this);
+    });
+}
+
+function get_input(row,name){
+    var input = 'input[name="order_rows['+row+']['+name+']"]';
+    return jQuery(input);
+}
+
+function change_sum(input){
+    var changed = input.name;
+    var row = changed.split('[')[1];
+    row = row.substr(0,row.length-1);
+    row = parseInt(row);
+    if(get_input(row,'price').val() != "" && get_input(row,'amount').val())
+        get_input(row,'sum').val(
+            Math.round((parseFloat(get_input(row,'price').val()) * parseFloat(get_input(row,'amount').val())) * 100) / 100
+            );
+    else{
+        get_input(row,'sum').val(0.00);
+    }
+}
+
